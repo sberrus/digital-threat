6bcd7dd195b8d83574630e7c8df3b8f63a7819f2
samdev <samuelberrus@gmail.com>
2022-06-02 20:27:46 +0200

    Adding sample buttons for appbar

--- src/components/system/TaskBar/AppsBar/AppsBar.tsx
+++ src/components/system/TaskBar/AppsBar/AppsBar.tsx
@@ -1,16 +1,16 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
+import AppsButtons from "./components/AppsButtons";
+import SampleLogo from "../../assets/sample/100x100.svg";
 
 const AppsBar = () => {
 	return (
 		<Col id="appsBar" sm={9} className="h-100 d-flex align-items-center justify-content-start">
-			<Row>
-				<Col as="button" className="btn btn-outline-success ms-2 h-75">
-					app1
-				</Col>
-				<Col as="button" className="btn btn-outline-success ms-2 h-75">
-					app2
-				</Col>
-			</Row>
+			<Container fluid className="h-100">
+				<AppsButtons logoRoute={SampleLogo} />
+				<AppsButtons logoRoute={SampleLogo} />
+				<AppsButtons logoRoute={SampleLogo} />
+				<AppsButtons logoRoute={SampleLogo} />
+			</Container>
 		</Col>
 	);
 };
---
+++ src/components/system/TaskBar/AppsBar/components/AppsButtons.tsx
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+
+type AppsButtonProps = {
+	logoRoute: string;
+};
+
+const AppsButtons = ({ logoRoute }: AppsButtonProps) => {
+	return (
+		<Button variant="outline-success" className="h-100 ms-1">
+			<img src={logoRoute} alt="sample Image" style={{ height: "100%" }} />
+		</Button>
+	);
+};
+
+export default AppsButtons;
